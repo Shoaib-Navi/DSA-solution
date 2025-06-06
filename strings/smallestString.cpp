b8af112240e1c7d8e3a74e5bdcc027d1bcdae3de
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-06-06 22:23:03 +0530

    2434. Using a Robot to Print the Lexicographically Smallest String

---
+++ strings/smallestString.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string robotWithString(string s) {
+        int n = s.size();
+        if (n == 0) return "";
+        
+        vector<char> min_suffix(n);
+        min_suffix[n-1] = s[n-1];
+        for (int i = n-2; i >= 0; --i)
+            min_suffix[i] = min(s[i], min_suffix[i+1]);
+        
+        stack<char> t;
+        string p;
+        
+        for (int i = 0; i < n; ++i) {
+            t.push(s[i]);
+            while (!t.empty() && (i == n-1 || t.top() <= min_suffix[i+1])) {
+                p.push_back(t.top());
+                t.pop();
+            }
+        }
+        
+        return p;
+    }
+};

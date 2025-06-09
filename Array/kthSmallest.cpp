b624369c09f89c1e8a16f88bc18d7c62dae4a606
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-06-09 21:55:37 +0530

    440. K-th Smallest in Lexicographical Order

---
+++ Array/kthSmallest.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findKthNumber(int n, int k) {
+        long long current = 1;
+        k--;
+        while (k > 0) {
+            int steps = calculateSteps(n, current, current + 1);
+            if (steps <= k) {
+                current += 1;
+                k -= steps;
+            } else {
+                current *= 10;
+                k -= 1;
+            }
+        }
+        return current;
+    }
+    
+private:
+    int calculateSteps(int n, long long n1, long long n2) {
+        int steps = 0;
+        while (n1 <= n) {
+            steps += min((long long)n + 1, n2) - n1;
+            n1 *= 10;
+            n2 *= 10;
+        }
+        return steps;
+    }
+};

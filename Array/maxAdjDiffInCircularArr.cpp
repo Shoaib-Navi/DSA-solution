ed70625772b93f345b337725df510286fc5915cb
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-06-12 22:53:56 +0530

    add class solution

--- Array/maxAdjDiffInCircularArr.cpp
+++ Array/maxAdjDiffInCircularArr.cpp
@@ -1,1 +1,19 @@
 // 3423. Maximum Difference Between Adjacent Elements in a Circular Array
+
+class Solution {
+public:
+    int maxAdjacentDistance(vector<int>& nums) {
+        int n = nums.size();
+        int maxDiff = 0;
+
+        for (int i = 0; i < n; ++i) {
+          
+            int next = (i + 1) % n;
+            int diff = abs(nums[i] - nums[next]);
+            
+            maxDiff = max(maxDiff, diff);
+        }
+
+        return maxDiff;
+    }
+};

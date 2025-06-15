94725d7f7a67c5c50195ec74e22238dcc0ad5db0
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-06-15 23:50:07 +0530

    1432. Max Difference You Can Get From Changing an Integer

---
+++ Array/Max DifferenceYouCanGetFromChangingInteger.cpp
@@ -0,0 +1,43 @@
+// 1432. Max Difference You Can Get From Changing an Integer
+
+class Solution {
+public:
+    int maxDiff(int num) {
+        string s = to_string(num);
+        
+        
+        string s_max = s;
+        for (char c : s) {
+            if (c != '9') {
+                char from = c;
+                for (char& ch : s_max) {
+                    if (ch == from) ch = '9';
+                }
+                break;
+            }
+        }
+        
+        
+        string s_min = s;
+        if (s[0] != '1') {
+            char from = s[0];
+            for (char& ch : s_min) {
+                if (ch == from) ch = '1';
+            }
+        } else {
+            for (int i = 1; i < s.size(); ++i) {
+                if (s[i] != '0' && s[i] != '1') {
+                    char from = s[i];
+                    for (char& ch : s_min) {
+                        if (ch == from) ch = '0';
+                    }
+                    break;
+                }
+            }
+        }
+        
+        int maxNum = stoi(s_max);
+        int minNum = stoi(s_min);
+        return maxNum - minNum;
+    }
+};

574b66f409cb6ac563140e2416d069fc6039ae95
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-06-18 22:34:15 +0530

    add main function

--- Array/DivideArrIntoArrsWithMaxDiff.cpp
+++ Array/DivideArrIntoArrsWithMaxDiff.cpp
@@ -16,3 +16,42 @@ public:
         return res;
     }
 };
+
+
+
+int main() {
+    vector<int> nums;
+    int n, k;
+
+    cout << "Enter the number of elements (multiple of 3): ";
+    cin >> n;
+
+    cout << "Enter the elements:\n";
+    for (int i = 0; i < n; ++i) {
+        int val;
+        cin >> val;
+        nums.push_back(val);
+    }
+
+    cout << "Enter k: ";
+    cin >> k;
+
+    Solution sol;
+    vector<vector<int>> result = sol.divideArray(nums, k);
+
+    if (result.empty()) {
+        cout << "[]\n";
+    } else {
+        cout << "Result:\n";
+        for (const auto& group : result) {
+            cout << "[";
+            for (int i = 0; i < group.size(); ++i) {
+                cout << group[i];
+                if (i < group.size() - 1) cout << ", ";
+            }
+            cout << "]\n";
+        }
+    }
+
+    return 0;
+}

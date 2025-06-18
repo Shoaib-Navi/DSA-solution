a8c0ce236fae65f14962683ea79b64445df75397
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-06-18 22:38:19 +0530

    add main function to the program

--- Array/maxAdjDiffInCircularArr.cpp
+++ Array/maxAdjDiffInCircularArr.cpp
@@ -17,3 +17,23 @@ public:
         return maxDiff;
     }
 };
+
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements:\n";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    Solution sol;
+    int result = sol.maxAdjacentDistance(nums);
+
+    cout << "Maximum difference between adjacent elements (circular): " << result << endl;
+
+    return 0;
+}
